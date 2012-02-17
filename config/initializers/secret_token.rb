dc4b0a1a401573d73fccc950d115d16935ff0477
jkao <jeff.kao.000@gmail.com>
2012-02-16 23:05:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readlett::Application.config.secret_token = '1252483451725d5ab6ce644f4e23d6f9a772786a5301bf17d6f39d83df40ad6cee0257b580ed0143815580217b382ebbca0726aa4edbe14957b89d4862f290c3'
